69b1cd62d1ff60c18d3674299e35df757a29c47e
viniciusbezerra0 <vinicius.bezerra@heritagecp.com.br>
2024-12-15 17:18:18 -0300

    Update bancos.py

--- bancos.py
+++ bancos.py
@@ -2,6 +2,16 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-df = pd.DataFrame([[2,4],[4,6],[0,5]])
+file_id = '1s8LmHMExpr1DgCm88FBZGc1Vxg9TxIjl'
+url = f'https://drive.google.com/uc?id={file_id}'
+output = 'capital.csv'  # Nome do arquivo que você quer salvar
 
-st.table(df)
+# Baixar o arquivo CSV do Google Drive
+gdown.download(url, output, quiet=False)
+
+# Carregar o CSV em um DataFrame
+capital_resumo = pd.read_csv(output)
+
+# Mostrar o DataFrame no Streamlit
+import streamlit as st
+st.write(capital_resumo)
