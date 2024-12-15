1005a768aeee776a7943feb6a674efe71f306ab1
viniciusbezerra0 <vinicius.bezerra@heritagecp.com.br>
2024-12-15 20:51:10 -0300

    Update bancos.py

--- bancos.py
+++ bancos.py
@@ -6,16 +6,15 @@ import gdown
 st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 
 capital_resumo = pd.read_csv("https://drive.google.com/uc?id=12xnRhBcTi_0bryw1OcwSl9V19VWNlEQy")
-'''
-resumo_financeiro = baixar("resumo_consolidado.csv", "14jyMjIe3P9HUUypmmpWd806XexYCtj-c")
-ativo = baixar("ativo.csv", "1R_y8vtNABZO35PeoyuvZouEZDYIuBJdm")
-passivo = baixar("passivo.csv", "1dhHMgSIC_bvdblg2OzMmr1kHYA7fYSIX")
-dre = baixar("dre.csv", "1eiRk3ZnPlSRMyp4ZoiFKg-M4d7MQmSBz")
-eh_historico = baixar("eh_historico.csv", "1Ss1pmXRrvxT0FH775ok9dBYogF3QLDxm")
-resumo_risco = baixar("carteira_risco_resumo.csv", "1P3uuEzSlofgdNH_TK04MxYZU2Q_Wq7QA")
-resumo_geografico = baixar("carteira_geografica_resumo.csv", "1nDsVB6rbBPTMkDP6jzSI8UT6hInDFVp7")
-carteira_pf = baixar("carteira_pf_resumo.csv", "1SYqSqDg_cYQMHSYzqP8p6PCM0-L9v5ha")
-carteira_pj = baixar("carteira_pj_resumo.csv", "1125I0cQmhvCR7_oO60PQmpZicC5wY9ld")
-perc_pf = baixar("perc_pf.csv", "1luCZeOTeLhoZ9N-mbNbNxW7pMN8h0IjR")
-'''
+resumo_financeiro = pd.read_csv("https://drive.google.com/uc?id=14jyMjIe3P9HUUypmmpWd806XexYCtj-c")
+ativo = pd.read_csv("https://drive.google.com/uc?id=1R_y8vtNABZO35PeoyuvZouEZDYIuBJdm")
+passivo = pd.read_csv("https://drive.google.com/uc?id=1dhHMgSIC_bvdblg2OzMmr1kHYA7fYSIX")
+dre = pd.read_csv("https://drive.google.com/uc?id=1eiRk3ZnPlSRMyp4ZoiFKg-M4d7MQmSBz")
+eh_historico = pd.read_csv("https://drive.google.com/uc?id=1Ss1pmXRrvxT0FH775ok9dBYogF3QLDxm")
+resumo_risco = pd.read_csv("https://drive.google.com/uc?id=1P3uuEzSlofgdNH_TK04MxYZU2Q_Wq7QA")
+resumo_geografico = pd.read_csv("https://drive.google.com/uc?id=1nDsVB6rbBPTMkDP6jzSI8UT6hInDFVp7")
+carteira_pf = pd.read_csv("https://drive.google.com/uc?id=1SYqSqDg_cYQMHSYzqP8p6PCM0-L9v5ha")
+carteira_pj = pd.read_csv("https://drive.google.com/uc?id=1125I0cQmhvCR7_oO60PQmpZicC5wY9ld")
+perc_pf = pd.read_csv("https://drive.google.com/uc?id=1luCZeOTeLhoZ9N-mbNbNxW7pMN8h0IjR")
+
 st.table(capital_resumo[:10])
