d5001d86079e3de1b18f644818db16aed1ade0c8
viniciusbezerra0 <vinicius.bezerra@heritagecp.com.br>
2024-12-15 19:30:24 -0300

    Update bancos.py

--- bancos.py
+++ bancos.py
@@ -5,10 +5,11 @@ import gdown
 
 st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 
-def baixar(arquivo, url):
+def baixar(arquivo, id):
+    url = f'https://drive.google.com/uc?id={id}'
     gdown.download(url, arquivo, quiet=False)
     return pd.read_csv(arquivo)
 
-ativo = baixar("ativo.csv", "https://drive.google.com/file/d/12xnRhBcTi_0bryw1OcwSl9V19VWNlEQy/view?usp=drive_link")
+ativo = baixar("ativo.csv", "1QKPGQiP6QIPQDMQg9Ygdlfg4l3snkZYI")
 
 st.table(ativo)
